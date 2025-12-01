981c666cfd56f0fdd1242593cc8e99d27b1fcc98
IraiDev <68446666+IraiDevPersonal@users.noreply.github.com>
2025-12-01 10:34:00 -0300

    Initial commit

---
+++ src/lib/config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import envVar from "env-var";
+
+export const ENVS = {
+	PORT: envVar.get("PORT").required().asPortNumber(),
+	LOG_LEVEL: envVar.get("LOG_LEVEL").default("info").asString(),
+	ALLOWED_ORIGINS: envVar.get("ALLOWED_ORIGINS").default("*").asArray(),
+	NODE_ENV: envVar.get("NODE_ENV").default("development").asString(),
+};
